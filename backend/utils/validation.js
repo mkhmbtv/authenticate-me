8060d6ea1d0fe9c7f1d66ac844c720ba3ebd8204
your name here <mmakhambetov@yandex.ru>
2021-12-10 23:06:28 +0400

    Add handleValidationErrors middleware

---
+++ backend/utils/validation.js
@@ -0,0 +1,17 @@
+const { validationResult } = require('express-validator');
+
+const handleValidationErrors = (req, res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors.array().map((error) => error.msg);
+    const err = new Error('Bad Request.')
+    err.errors = errors;
+    err.title = 'Bad Request.';
+    err.status = 400;
+    return next(err);
+  }
+  next();
+};
+
+module.exports = { handleValidationErrors };
