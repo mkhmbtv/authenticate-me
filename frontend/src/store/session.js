372bbb5185b04272d00b7a8a7411e1d71df532d8
your name here <mmakhambetov@yandex.ru>
2021-12-11 21:12:55 +0400

    Create sessionReducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,41 @@
+import { csrfFetch } from "./csrf"
+
+const SET_USER = 'session/SET_USER';
+const REMOVE_USER = 'session/REMOVE_USER';
+
+const setUser = (user) => {
+  return { 
+    type: SET_USER,
+    user,
+  };
+}
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const login = ({ credential, password }) => async (dispatch) => {
+  const res = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({ credential, password }),
+  });
+
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
+};
+
+const initialState = { user: null };
+
+export default function sessionReducer (state = initialState, action) {
+  switch (action.type) {
+    case SET_USER:
+      return { ...state, user: action.user };
+    case REMOVE_USER: 
+      return { ...state, user: null }
+    default:
+      return state;
+  }
+};
