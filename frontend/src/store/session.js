8e4679cf3b24be14fd22a7625fa405278d0b067f
your name here <mmakhambetov@yandex.ru>
2021-12-12 14:33:32 +0400

    Create logout action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -45,6 +45,15 @@ export const signup = ({ username, email, password }) => async (dispatch) => {
   return res;
 };
 
+export const logout = () => async (dispatch) => {
+  const res = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+
+  dispatch(removeUser());
+  return res;
+};
+
 const initialState = { user: null };
 
 export default function sessionReducer (state = initialState, action) {
