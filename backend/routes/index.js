2fcbc6d4d48684e6bc1edb03b4f53621a9a15e77
your name here <mmakhambetov@yandex.ru>
2021-12-13 17:05:49 +0400

    Set up Express + React app

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'production') {
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../../frontend/', 'build', 'index.html')
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../../frontend/', 'build', 'index.html')
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
